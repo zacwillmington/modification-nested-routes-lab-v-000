d2fcb8f1e2893b3bea0a456d9dc6684b05a33f22
Zac Willmington <zac.willmington@gmail.com>
2018-07-20 23:26:31 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -45,7 +45,6 @@ class SongsController < ApplicationController
   def edit
       if params[:artist_id]
           @artist = Artist.find_by(:id => params[:artist_id])
-
           if @artist.nil?
               redirect_to artists_path(@artist)
           end
