d5a504ad7d5be1ce339a5c45366d3eb85d7784c0
Zac Willmington <zac.willmington@gmail.com>
2018-07-20 20:50:42 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,11 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+      if params[:artist_id] && Artist.exists?(params[:artist_id])
+          @song = Song.new(artist_id: params[:artist_id])
+      else
+          redirect_to artists_path
+      end
   end
 
   def create
@@ -39,7 +43,16 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+      binding.pry
+      if params[:artist_id]
+          artist = Artist.find_by(:id => params[:artist_id])
+          if artist.nil?
+              redirect_to artists_path
+          else
+                
+              @song = Song.find(params[:id])
+          end
+      end
   end
 
   def update
@@ -64,7 +77,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
-
